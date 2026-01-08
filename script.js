d98698af65031e51a93b433185689c05292d1633
irefmi <arrowstar51@gmail.com>
2026-01-08 18:36:28 +0100

    Added script.js, cause I needed to add an hamburger for smaller screen viewing

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+const toggleBtn = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.nav');
+
+toggleBtn.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
+
